6782827d90e87f78b2b41ad3bac6b145a0ce9880
Edric Guo <edricguo@Umich.edu>
2018-02-23 10:40:13 -0500

    Implemented too many params.

--- rpn.py
+++ rpn.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 
+import operator
+
+operators = {
+    '+': operator.add,
+    '-': operator.sub,
+}
 def calculate(arg):
     stack = list()
     for token in arg.split():
@@ -7,19 +13,14 @@ def calculate(arg):
             value = int(token)
             stack.append(value)
         except ValueError:
-            if token == '+':
-                arg1 = stack.pop()
-                arg2 = stack.pop()
-                result = arg1 + arg2
-                stack.append(result)
-            elif token == '-':
-                arg2 = stack.pop()
-                arg1 = stack.pop()
-                result = arg1 - arg2
-                stack.append(result)
-            else:
-                print('Not an operator.')
-        print(stack) 
+            function = operators[token]
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = function(arg1, arg2)
+            stack.append(result)
+        print(stack)
+    if len(stack) != 1:
+        raise TypeError
     return stack.pop()
 def main():
     while True:
